eb686d22a4c22e57c508ad9aeed626efeada1267
LiorBreitman8234 <liorbreitman321@gmail.com>
2021-12-06 13:45:02 +0200

    optimize dijkstra

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         GraphAlgorithms graph = new GraphAlgorithms();
-        graph.load("data/G4.json");
+        graph.load("/home/bravo8234/Desktop/study/OOP/Ex2/Json_files/G4.json");
         long startTime = System.currentTimeMillis();
         System.out.println(graph.center().getKey());
         long finishTime = System.currentTimeMillis();
