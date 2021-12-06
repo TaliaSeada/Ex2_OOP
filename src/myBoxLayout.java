7fdaf5dbb0e2766f8f53f6f0557fec0434d5430f
LiorBreitman8234 <liorbreitman321@gmail.com>
2021-12-06 11:56:01 +0200

    1. Continued working on gui 2. added class files for gui

--- src/myBoxLayout.java
+++ src/myBoxLayout.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicArrowButton;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class myBoxLayout{
+    public final boolean loaded = false;
     public static void createMainWindow(Container pane) {
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
         JButton fileActions = new JButton("press to do actions from files");
@@ -14,7 +14,7 @@ public class myBoxLayout{
         fileActions.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                CreateFileWindow(pane);
+                CreateFileWindow();
             }
         });
 
@@ -29,10 +29,42 @@ public class myBoxLayout{
         pane.add(algoActions);
     }
 
-    public static void CreateFileWindow(Container pane){
+    public static void CreateFileWindow(){
+        JFrame frame = new JFrame("file actions window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
         String Path = "";
-        pane.setLayout(new GroupLayout(pane));
+        GroupLayout layout = new GroupLayout(frame.getContentPane());
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        JTextField path = new JTextField("Enter path of file (instead of this)");
+
         JButton load = new JButton("load from file");
+
+
+
+        JButton save = new JButton("Save to file");
+
+        layout.setHorizontalGroup(
+                layout.createSequentialGroup()
+                        .addComponent(save)
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(path)
+                                .addComponent(load))
+        );
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(save)
+                                .addComponent(load))
+                        .addComponent(path)
+        );
+
+        frame.setVisible(true);
+
+
     }
 
 
