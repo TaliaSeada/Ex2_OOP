3696f24aa83528efa7d3dbd72b40a9a2aa535f90
LiorBreitman8234 <liorbreitman321@gmail.com>
2021-12-07 18:15:49 +0200

    work on gui

--- src/myBoxLayout.java
+++ src/myBoxLayout.java
@@ -4,12 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class myBoxLayout{
-    public static boolean loaded = false;
+//    public static boolean loaded = false;
     private static GraphAlgorithms GA = new GraphAlgorithms();
     public static void createMainWindow(Container pane) {
         GA.load("data/G1.json");
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        JButton fileActions = new JButton("Files");
+        JButton fileActions = new JButton("file");
         fileActions.setAlignmentX(Component.CENTER_ALIGNMENT);
         pane.add(fileActions);
         pane.add(Box.createRigidArea(new Dimension(5, 20)));
@@ -26,26 +26,26 @@ public class myBoxLayout{
         pane.add(graphActions);
         pane.add(Box.createRigidArea(new Dimension(5, 20)));
 
-        graphActions.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(!loaded){
-                    JOptionPane.showMessageDialog(pane, "You did not load any file!");
-                }
-            }
-        });
+//        graphActions.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                if(!loaded){
+//                    JOptionPane.showMessageDialog(pane, "You did not load any file!");
+//                }
+//            }
+//        });
 
         JButton algoActions = new JButton("Algorithms");
         algoActions.setAlignmentX(Component.CENTER_ALIGNMENT);
         pane.add(algoActions);
-        algoActions.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(!loaded){
-                    JOptionPane.showMessageDialog(pane, "You did not load any file!");
-                }
-            }
-        });
+//        algoActions.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                if(!loaded){
+//                    JOptionPane.showMessageDialog(pane, "You did not load any file!");
+//                }
+//            }
+//        });
     }
 
     public static void CreateFileWindow(){
@@ -80,7 +80,6 @@ public class myBoxLayout{
         layout.setHorizontalGroup(
                 layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(load)
                                 .addComponent(path))
                         .addComponent(save)
 
@@ -89,8 +88,7 @@ public class myBoxLayout{
         layout.setVerticalGroup(
                 layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(save)
-                                .addComponent(load))
+                                .addComponent(save))
                         .addComponent(path)
         );
 
