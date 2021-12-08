9453b8417a2efd3e02dfb963cf97bacb5826be86
LiorBreitman8234 <liorbreitman321@gmail.com>
2021-12-08 16:06:09 +0200

    continued working on gui

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         GraphAlgorithms graph = new GraphAlgorithms();
-        graph.load("data/G1.json");
+        graph.load("data/G2.json");
         List<NodeData> check = new ArrayList<>();
         check.add(graph.getGraph().getNode(0));
         check.add(graph.getGraph().getNode(3));
@@ -15,12 +15,12 @@ public class Main {
 
         //System.out.println(graph.isConnected());
         long start = System.currentTimeMillis();
-        List<NodeData> path = graph.tsp(check);
+        graph.center();
         long finish = System.currentTimeMillis();
         System.out.println("time = " + (finish-start));
-        for(int i =0; i < path.size();i++){
-            System.out.println(path.get(i).getKey());
-        }
+//        for(int i =0; i < path.size();i++){
+//            System.out.println(path.get(i).getKey());
+//        }
 
 
 
