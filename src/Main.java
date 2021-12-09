b2a231f1fffe5d2a0d721ddb531a3c646167e7d8
LiorBreitman8234 <liorbreitman321@gmail.com>
2021-12-09 19:03:37 +0200

    class file

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,9 @@ public class Main {
     public static void main(String[] args) {
         GraphAlgorithms graph = new GraphAlgorithms();
 //        graph.load("C:\\10000Nodes.json");
-       graph.load("data/G4.json");
-       graph.getGraph().removeNode(362);
-        graph.getGraph().removeNode(56);
-        graph.getGraph().removeNode(320);
-        graph.getGraph().removeNode(264);
-        graph.getGraph().removeNode(407);
-        graph.getGraph().removeNode(288);
-        graph.getGraph().removeNode(325);
-        graph.getGraph().removeNode(645);
-        graph.getGraph().removeNode(55);
-        graph.getGraph().removeNode(203);
-
-
-
-
-
-
-
-
-
-        System.out.println(graph.center().getKey());
+       graph.load("data/G3.json");
+       graph.getGraph().removeNode(40);
+        System.out.println(graph.dijkstra(1).get(0));
         //System.out.println(args[0]);
     }
 }
